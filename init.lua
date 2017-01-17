412a3081e820ea2b4e7e39e05ce3f9fb72f7dca9
jordan4ibanez <jordan4ibanez@gmail.com>
2017-01-17 01:09:44 -0500

    Push new size

--- init.lua
+++ init.lua
@@ -1,15 +1,16 @@
 minetest.register_node("newhand:hand", {
 	description = "",
-	tiles = {"character.png"},--^[transformR180"},
+	tiles = {"character.png"},
 	on_place = function(itemstack, placer, pointed_thing)
 		--do nothing
 	end,
 	visual_scale = 1,
-	wield_scale = {x=1,y=1,z=1},
+	wield_scale = {x=0.7,y=0.7,z=0.7},
 	paramtype = "light",
 	drawtype = "mesh",
 	mesh = "hand.b3d",
 	node_placement_prediction = false,
+	visible = false,
 })
 
 minetest.register_on_joinplayer(function(player)
