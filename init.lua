d18c2bfad8fa22022e205d791bcd811822180e44
jordan4ibanez <jordan4ibanez@gmail.com>
2017-01-22 17:15:16 -0500

    Functionality with simple skins

--- init.lua
+++ init.lua
@@ -1,19 +1,65 @@
-minetest.register_node("newhand:hand", {
-	description = "",
-	tiles = {"character.png"},
-	on_place = function(itemstack, placer, pointed_thing)
-		return
-	end,
-	visual_scale = 1,
-	wield_scale = {x=1,y=1,z=1},
-	paramtype = "light",
-	drawtype = "mesh",
-	mesh = "hand.b3d",
-	node_placement_prediction = "",
-})
+--print(dump(skins.list))
 
-minetest.register_on_joinplayer(function(player)
-	player:get_inventory():set_stack("hand", 1, "newhand:hand")
-end)
+local simple_skins = minetest.get_modpath("simple_skins") ~= nil
 
 
+
+--simple skins is enabled
+if simple_skins == true then
+	newhand_oldskin = {}
+	--generate a node for every skin
+	for _,texture in pairs(skins.list) do
+		minetest.register_node("newhand:"..texture, {
+			description = "",
+			tiles = {texture..".png"},
+			on_place = function(itemstack, placer, pointed_thing)
+				return
+			end,
+			visual_scale = 1,
+			wield_scale = {x=1,y=1,z=1},
+			paramtype = "light",
+			drawtype = "mesh",
+			mesh = "hand.b3d",
+			node_placement_prediction = "",
+		})
+	end
+	--change the player's hand to their skin
+	minetest.register_on_joinplayer(function(player)
+		local skin = skins.skins[player:get_player_name()]
+		player:get_inventory():set_stack("hand", 1, "newhand:"..skin)
+	end)
+	
+	--check to update the skin
+	minetest.register_globalstep(function(dtime)
+		for _,player in ipairs(minetest.get_connected_players()) do
+			local name = player:get_player_name()
+			local skin = skins.skins[name]
+			
+			if skin ~= newhand_oldskin[name] then
+				player:get_inventory():set_stack("hand", 1, "newhand:"..skin)
+			end
+			
+			newhand_oldskin[name] = skin
+		end
+	end)
+	
+--do default skin if no skin mod installed
+else
+	minetest.register_node("newhand:hand", {
+		description = "",
+		tiles = {"character.png"},
+		on_place = function(itemstack, placer, pointed_thing)
+			return
+		end,
+		visual_scale = 1,
+		wield_scale = {x=1,y=1,z=1},
+		paramtype = "light",
+		drawtype = "mesh",
+		mesh = "hand.b3d",
+		node_placement_prediction = "",
+	})
+
+	minetest.register_on_joinplayer(function(player)
+		player:get_inventory():set_stack("hand", 1, "newhand:hand")
+	end)
+end
